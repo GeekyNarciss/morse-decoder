7061d1397cf0e2e3e1a5a9fac30d8a367a0b7095
Artemiy <GeekyNarciss@users.noreply.github.com>
2020-03-22 16:06:24 +0300

    feat: solution

--- src/index.js
+++ src/index.js
@@ -38,7 +38,34 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    expr = String(expr);
+    let result = '';
+    let i = 0;
+    while (i < expr.length) {
+        if (expr[i] !== '*') {
+            let encoded = expr.substr(i, 10);
+            let temp = '';
+            for (let s = 0; s < encoded.length; s++) {
+                if (encoded[s] !== '0') {
+                    let twoS = encoded[s] + encoded[++s];
+                    switch (twoS) {
+                        case '10': {
+                            temp += '.';
+                            break;
+                        }
+                        case '11': {
+                            temp += '-';
+                        }
+                    }
+                }
+            }
+            result += MORSE_TABLE[temp];
+        } else {
+            result += ' ';
+        }
+        i += 10;
+    }
+    return result;
 }
 
 module.exports = {
